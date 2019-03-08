8c66902a0c9d0563eb24b4aa2287eb61a5207497
Aman <meet.aman7@gmail.com>
2019-03-07 18:51:38 -0600

    Servant scaffolding

---
+++ src/Server.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Server where
+
+import Data.Aeson
+import Models
+import Servant
+
+type ReviewAPI1 = "pastes" :> Get '[JSON] [Paste]
+
+server1 :: Server ReviewAPI1
+server1 = return pastes1
+
+reviewAPI :: Proxy ReviewAPI1
+reviewAPI = Proxy
+
+app1 :: Application
+app1 = serve reviewAPI server1
+
+pastes1 :: [Paste]
+pastes1 = undefined
