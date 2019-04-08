569689448777b0c7c45cc7d51867bd3cd330077f
Aman <meet.aman7@gmail.com>
2019-04-07 20:20:36 -0500

    Relax cors settings

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
+{-# Language OverloadedStrings #-}
+
 module Main where
 
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
 import Server
 import qualified Db
 
 main :: IO ()
 main = do
     db <- Db.makeDb []
-    run 8081 $ app2 db
+    run 8081 $ handleCors $ app2 db
+
+handleCors = cors ( const $
+  Just (simpleCorsResourcePolicy
+    { corsRequestHeaders = ["Content-Type"]
+    }) )
+
