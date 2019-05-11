7346b3e9199e14de8904c65a6bbbcb04da431dfb
Aman <meet.aman7@gmail.com>
2019-05-11 13:41:12 -0500

    Add server http logging

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Network.Wai.Handler.Warp
+import Network.Wai.Logger
 import Network.Wai.Middleware.Cors
 import Server
 import qualified Db
@@ -10,7 +11,9 @@ import qualified Db
 main :: IO ()
 main = do
     db <- Db.makeDb []
-    run 8081 $ handleCors $ app2 db
+    withStdoutLogger $ \aplogger -> do
+      let settings = setPort 8081 $ setLogger aplogger defaultSettings
+      runSettings settings $ handleCors $ app2 db
 
 handleCors = cors ( const $
   Just (simpleCorsResourcePolicy
